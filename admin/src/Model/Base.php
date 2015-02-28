1519104ed819596fea864e683d85ace1162ddcb2
David Lima <dave@blogdodave.com.br>
2015-02-28 20:22:03 -0300

    Adicionado @return self nos métodos getById e get

--- admin/src/Model/Base.php
+++ admin/src/Model/Base.php
@@ -1,21 +1,21 @@
 <?php
 /**
  ************************************************************************
-Copyright [2014] [David Lima]
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-************************************************************************
-*/
+ Copyright [2014] [David Lima]
+ 
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+ 
+ http://www.apache.org/licenses/LICENSE-2.0
+ 
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+ ************************************************************************
+ */
 namespace Model;
 
 use \Lib\Data;
@@ -184,7 +184,7 @@ abstract class Base
      *
      * @param integer $id            
      * @throws \Exception
-     * @return mixed
+     * @return self
      */
     public function getById($id = false)
     {
@@ -202,7 +202,7 @@ abstract class Base
      *
      * @param string $column            
      * @param string $value            
-     * @return mixed
+     * @return self
      */
     public function getByColumn($column, $value)
     {
